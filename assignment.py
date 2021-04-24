e8f7fa1d97dfec9cf064e5d257bd5b0d184ad473
Uğurcan YILMAZ <33160207+Elijah0704@users.noreply.github.com>
2021-04-24 11:56:03 +0300

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,7 @@
+#  Assignment-008/1 (Password Reminder)
+my_name = "Elijah"
+name = input("Please enter your name: ")
+if name == "Elijah" :
+    print("Hello,",my_name,"! The password is : W@12")
+else :
+    print("Hello,",name,"! See you later.")
