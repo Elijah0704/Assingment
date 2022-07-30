c8a088a8aaf68ac55ab34d883463562c8e6dbf96
Uğurcan YILMAZ <33160207+ugrcnylmz@users.noreply.github.com>
2022-07-30 12:07:34 +0300

    Delete assignment.py

--- assignment.py
+++
@@ -1,7 +0,0 @@
-#  Assignment-008/1 (Password Reminder)
-my_name = "Elijah"
-name = input("Please enter your name: ")
-if name == "Elijah" :
-    print("Hello,",my_name,"! The password is : W@12")
-else :
-    print("Hello,",name,"! See you later.")
